2bf833c7524e21867590c48a0f61941b7bb8552f
Andrei Freire <andrei@Andreis-MacBook-Pro.local>
2018-09-30 13:28:59 -0400

    final commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
 $(function() {
-console.log("ready");
-$('fa-bars').on('click', function(){
-console.log("whyyyy");
-	});
-	$('fa-bars').on('click', function(){
+
+
+	$('.fa-bars').on('click', function(){
 		console.log("do i get here");
+		if($('ul.mainNav.open').val()!= undefined)
+			$('ul.mainNav').removeClass('open');
+		else
 		$('ul.mainNav').addClass('open');
 	});
+
+
+
+
 	$('ul.mainNav li a').on('click', function(){
 		$('ul.mainNav').removeClass('open');
 	});
